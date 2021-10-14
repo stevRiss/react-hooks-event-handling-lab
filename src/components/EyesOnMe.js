7cfa0b683ff5a60935a8ec36feed0dcad40ec88f
Stephen <stevriss22@gmail.com>
2021-10-14 17:09:30 -0400

    finished

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,15 @@
 // Code EyesOnMe Component Here
+function EyesOnMe(){
+    const HandleBlur = () => {
+        return console.log('Hey! Eyes on me!')
+        
+    }
+    const HandleFocus = () => {
+        return console.log('Good!')
+    }
+    return(
+        <button onBlur ={HandleBlur} onFocus = {HandleFocus}>Eyes on me</button>
+    );
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,18 @@
 // Code Keypad Component Here
+function Keypad(){
+    function Handler(event){
+        console.log('Entering password...')
+        
+    }
+    return(
+        <input 
+        type='password' 
+        name ='number'
+        onChange = {Handler}
+        >
+        
+        </input>
+    );
+}
+
+export default Keypad;
